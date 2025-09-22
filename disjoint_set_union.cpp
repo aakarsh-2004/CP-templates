f1a891ba0f5387a1a336fdc563980bf27617307c
aakarsh-2004 <aakarshbeohar2004@gmail.com>
2025-09-23 00:14:55 +0530

    template for dsu changed

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -1,10 +1,11 @@
 class DisjointSet {
-    vector<int> parent, rank;
+    vector<int> parent, rank, size;
 
     public:
     DisjointSet(int n) {
         parent.resize(n + 1, 0);
         rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
 
         for(int i = 0; i <= n; i++) parent[i] = i;
     }
@@ -35,4 +36,23 @@ class DisjointSet {
 
         return true;
     }
-}
+
+    bool unionBySize(int u, int v) {
+        int upu = findUParent(u), upv = findUParent(v);
+
+        if(upu == upv) return false;
+
+        int size1 = size[upu], size2 = size[upv];
+
+        if(size1 < size2) {
+            parent[upu] = upv;
+            size[upv] += size[upu];
+        }
+        else {
+            parent[upv] = upu;
+            size[upu] += size[upv];
+        }
+
+        return true;
+    }
+};
